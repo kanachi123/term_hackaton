6f4dc7cc777a9d0fe5168f3dfcac6faf595f22f4
kanachi123 <shrekkanachyan1234@gmail.com>
2025-11-09 16:21:43 +0400

    v3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './frontend/App.jsx';
 import './frontend/styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
